95a5ea74df7fdc1ef46935c5230b221f4db1fd7a
Kolaposki <oshodikolapo@gmail.com>
2022-09-21 12:46:32 +0100

    :pencil2: :hammer: Refactor and update models

--- client/models.py
+++ client/models.py
@@ -4,6 +4,8 @@ from django.db import models
 from drf_user.models import User
 import datetime
 from django.conf import settings
+from django.utils.text import slugify
+import uuid
 
 
 class Profile(models.Model):
@@ -31,10 +33,11 @@ class Profile(models.Model):
 class Product(models.Model):
     title = models.CharField(max_length=250)
     category = models.CharField(max_length=250)
-    price = models.FloatField()
+    picture = models.ImageField(upload_to="products/images", null=True, blank=True)
+    price = models.DecimalField(decimal_places=2, max_digits=20, default=0)
+    quantity = models.IntegerField(default=10)  # available quantity of given product
     description = models.TextField()
-    slug = models.SlugField()
-    product_image = models.ImageField(upload_to="products/%Y/%m/%d", null=True)
+    slug = models.SlugField(default='', editable=False, )
     available = models.BooleanField(default=True)
     added_on = models.DateTimeField(auto_now_add=True, null=True)
     update_on = models.DateTimeField(auto_now=True, null=True)
@@ -42,34 +45,46 @@ class Product(models.Model):
     def __str__(self):
         return self.title
 
+    @property
+    def is_available(self):
+        return self.quantity > 0
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Product, self).save(*args, **kwargs)
+
     # def get_absolute_url(self):
-    #     return reverse("client:product", kwargs={
-    #         'slug': self.slug
-    #     })
+    #     return reverse("product_detail", kwargs={"slug": self.slug})
 
 
 class OrderProduct(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL,
-                             on_delete=models.CASCADE)
+    """
+    Order made on a single product
+    """
+    customer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     ordered = models.BooleanField(default=False)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.IntegerField(default=1)
 
     def __str__(self):
-        return f"{self.quantity} of {self.item.title}"
+        return f"{self.quantity} of {self.product.title}"
 
-    def get_total_item_price(self):
+    def get_total_product_price(self):
         return self.quantity * self.product.price
 
 
 class Order(models.Model):
+    """
+    Total order of all Products
+    """
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
                              on_delete=models.CASCADE)
-    ref_code = models.CharField(max_length=20, blank=True, null=True)
     products = models.ManyToManyField(OrderProduct)
     start_date = models.DateTimeField(auto_now_add=True)
     ordered_date = models.DateTimeField()
     ordered = models.BooleanField(default=False)
+
+    # We assume there's only one delivery method. Home delivery
     shipping_address = models.ForeignKey(
         'Address', related_name='shipping_address', on_delete=models.SET_NULL, blank=True, null=True)
     billing_address = models.ForeignKey(
@@ -82,24 +97,13 @@ class Order(models.Model):
     refund_requested = models.BooleanField(default=False)
     refund_granted = models.BooleanField(default=False)
 
-    '''
-    1. Product added to cart
-    2. Adding a billing address
-    (Failed checkout)
-    3. Payment
-    (Preprocessing, processing, packaging etc.)
-    4. Being delivered
-    5. Received
-    6. Refunds
-    '''
-
     def __str__(self):
-        return self.user.username
+        return f"{self.user.username}'s Order"
 
     def get_total(self):
         total = 0
-        for order_item in self.products.all():
-            total += order_item.get_final_price()
+        for order_product in self.products.all():
+            total += order_product.get_total_product_price()
 
         return total
 
@@ -114,18 +118,30 @@ class Address(models.Model):
     default = models.BooleanField(default=False)
 
     def __str__(self):
-        return self.user.username
+        return f"{self.street_address} - {self.user.username}"
 
     class Meta:
         verbose_name_plural = 'Addresses'
 
 
 class Payment(models.Model):
-    charge_id = models.CharField(max_length=50)  # payment provider id eg stripe
+    METHOD = (
+        ("CARD", "Card"),
+        ("CASH", "Cash"),
+    )
+
+    charge_id = models.CharField(max_length=50, blank=True, null=True)  # payment provider id eg stripe
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
                              on_delete=models.SET_NULL, blank=True, null=True)
     amount = models.FloatField()
+    method = models.CharField(max_length=10, choices=METHOD)
     timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.user.username
+        return f"{self.amount} - {self.user.username}"
+
+    def save(self, *args, **kwargs):
+        if not self.charge_id:
+            self.charge_id = uuid.uuid4()
+
+        super(Payment, self).save(*args, **kwargs)
