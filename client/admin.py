d40c41778968cee8cefbbbf8f45a86f6c8167492
Kolaposki <oshodikolapo@gmail.com>
2022-09-21 11:42:52 +0100

    :heavy_plus_sign: Add product and order to admin

--- client/admin.py
+++ client/admin.py
@@ -1,3 +1,60 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Product, Order,OrderProduct, Payment, Address, Profile
+
+
+def make_refund_accepted(modeladmin, request, queryset):
+    queryset.update(refund_requested=False, refund_granted=True)
+
+
+make_refund_accepted.short_description = 'Update orders to refund granted'
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['user',
+                    'ordered',
+                    'being_delivered',
+                    'received',
+                    'refund_requested',
+                    'refund_granted',
+                    'shipping_address',
+                    'billing_address',
+                    'payment',
+                    ]
+    list_display_links = [
+        'user',
+        'shipping_address',
+        'billing_address',
+        'payment',
+    ]
+    list_filter = ['ordered',
+                   'being_delivered',
+                   'received',
+                   'refund_requested',
+                   'refund_granted']
+    search_fields = [
+        'user__username',
+        'ref_code'
+    ]
+    actions = [make_refund_accepted]
+
+
+class AddressAdmin(admin.ModelAdmin):
+    list_display = [
+        'user',
+        'street_address',
+        'apartment_address',
+        'country',
+        'zip',
+        'default'
+    ]
+    list_filter = ['default', 'country']
+    search_fields = ['user', 'street_address', 'apartment_address', 'zip']
+
+
+admin.site.register(Product)
+admin.site.register(OrderProduct)
+admin.site.register(Order, OrderAdmin)
+admin.site.register(Payment)
+admin.site.register(Address, AddressAdmin)
+admin.site.register(Profile)
