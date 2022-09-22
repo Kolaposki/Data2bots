c276915de307e2bcbb8d513fb0f97a6eb4074d90
Kolaposki <oshodikolapo@gmail.com>
2022-09-22 13:21:30 +0100

    :pencil2: Refactor models to remove unnecessary fields

--- client/models.py
+++ client/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 # from django.contrib.auth.models import User
 from drf_user.models import User
-import datetime
 from django.conf import settings
 from django.utils.text import slugify
 import uuid
@@ -83,21 +82,12 @@ class Order(models.Model):
                               on_delete=models.CASCADE)
     products = models.ManyToManyField(OrderProduct)
     start_date = models.DateTimeField(auto_now_add=True)
-    ordered_date = models.DateTimeField()
+    ordered_date = models.DateTimeField(blank=True, null=True)
     ordered = models.BooleanField(default=False)
 
     # We assume there's only one delivery method. Home delivery
-    shipping_address = models.ForeignKey(
-        'Address', related_name='shipping_address', on_delete=models.SET_NULL, blank=True, null=True)
-    billing_address = models.ForeignKey(
-        'Address', related_name='billing_address', on_delete=models.SET_NULL, blank=True, null=True)
-    payment = models.ForeignKey(
-        'Payment', on_delete=models.SET_NULL, blank=True, null=True)
-
-    being_delivered = models.BooleanField(default=False)
-    received = models.BooleanField(default=False)
-    refund_requested = models.BooleanField(default=False)
-    refund_granted = models.BooleanField(default=False)
+    address = models.ForeignKey('Address', on_delete=models.SET_NULL, blank=True, null=True)
+    payment = models.ForeignKey('Payment', on_delete=models.SET_NULL, blank=True, null=True)
 
     def __str__(self):
         return f"{self.buyer.username}'s Order"
@@ -142,6 +132,7 @@ class Payment(models.Model):
     method = models.CharField(max_length=10, choices=METHOD)
     timestamp = models.DateTimeField(auto_now_add=True)
 
+    # todo: deduct amount when class is called
     def __str__(self):
         return f"{self.amount} - {self.user.username}"
 
